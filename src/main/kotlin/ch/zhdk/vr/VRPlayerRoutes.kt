4aa27d64a53092fce1de8270635d3d54bce3c7a2
Florian <florian@bildspur.ch>
2020-02-03 22:26:13 +0100

    added basic structure of the server

---
+++ src/main/kotlin/ch/zhdk/vr/VRPlayerRoutes.kt
@@ -0,0 +1,45 @@
+package ch.zhdk.vr
+
+import io.ktor.application.call
+import io.ktor.http.content.*
+import io.ktor.locations.KtorExperimentalLocationsAPI
+import io.ktor.locations.Location
+import io.ktor.locations.get
+import io.ktor.response.respond
+import io.ktor.response.respondFile
+import io.ktor.response.respondText
+import io.ktor.routing.Route
+import java.io.File
+import java.io.FileInputStream
+import javax.print.DocFlavor
+import javax.print.SimpleDoc
+import javax.print.attribute.HashPrintRequestAttributeSet
+import javax.print.attribute.standard.Copies
+
+
+@KtorExperimentalLocationsAPI
+@Location("api/test/{id}/")
+data class TestRoute(val id: String)
+
+@KtorExperimentalLocationsAPI
+fun Route.vrPlayerRoutes() {
+
+    // webapp routes
+    static("app/") {
+        staticRootFolder = File("app/dist") // Establishes a root folder
+        default("index.html")
+        file("bundle.js", "bundle.js")
+        file("main.css", "main.css")
+        static("libs") {
+            files("libs")
+        }
+        static("assets") {
+            files("assets")
+        }
+    }
+
+    // api routes
+    get<TestRoute> { id ->
+        call.respond("Test id was: $id")
+    }
+}
---
+++ src/main/kotlin/ch/zhdk/vr/VRPlayerServer.kt
@@ -0,0 +1,55 @@
+package ch.zhdk.vr
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import io.ktor.application.install
+import io.ktor.features.*
+import io.ktor.gson.GsonConverter
+import io.ktor.http.ContentType
+import io.ktor.locations.KtorExperimentalLocationsAPI
+import io.ktor.locations.Locations
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+@KtorExperimentalLocationsAPI
+class VRPlayerServer {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            VRPlayerServer().start(args)
+        }
+    }
+
+    val gson: Gson = GsonBuilder()
+        .setPrettyPrinting()
+        .excludeFieldsWithoutExposeAnnotation()
+        .serializeSpecialFloatingPointValues()
+        .create()
+
+
+    fun start(args: Array<String>) {
+        val server = embeddedServer(Netty, port = 8900) {
+            install(DefaultHeaders)
+            install(CallLogging)
+            install(Locations)
+            install(ConditionalHeaders)
+            install(Compression) {
+                default()
+                excludeContentType(ContentType.Video.Any)
+            }
+            install(CORS)
+            {
+                anyHost()
+            }
+            install(ContentNegotiation) {
+                register(ContentType.Application.Json, GsonConverter(gson))
+            }
+
+            routing {
+                vrPlayerRoutes()
+            }
+        }
+        server.start()
+    }
+}
