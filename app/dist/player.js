fbb4e9dcea7bb6f9431ac2eca7d0705dd08367ba
Florian <florian@bildspur.ch>
2020-02-04 16:22:28 +0100

    added all needed deps

--- app/dist/player.js
+++ app/dist/player.js
@@ -1,6 +1,5 @@
 var options = { fluid: true };
 
 var player = videojs('video', options, function onPlayerReady() {
-    videojs.log('Your player is ready!');
-    this.vr({projection: '360'});
+    this.vr({projection: '360', forceCardboard: true});
 });
